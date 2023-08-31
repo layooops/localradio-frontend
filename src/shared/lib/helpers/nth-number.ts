4a62d764500e8e389335abafb32bcf6c7a828189
Vitalii Astakhov <vitalastakhov@gmail.com>
2023-08-31 15:30:07 +0300

    refactor: update nthNumber function for clarity
    Refactor the nthNumber function to improve readability and maintainability. Replace numeric suffixes with named constants from the NumberSuffix enum.

--- src/shared/lib/helpers/nth-number.ts
+++ src/shared/lib/helpers/nth-number.ts
@@ -1,13 +1,25 @@
+export enum NumberSuffix {
+  FIRST = 'st',
+  SECOND = 'nd',
+  THIRD = 'rd',
+  OTHER = 'th',
+}
+
 export const nthNumber = (number: number) => {
-  if (number > 3 && number < 21) return 'th';
-  switch (number % 10) {
+  if (number > 3 && number < 21) {
+    return NumberSuffix.OTHER;
+  }
+
+  const lastDigit = number % 10;
+
+  switch (lastDigit) {
     case 1:
-      return 'st';
+      return NumberSuffix.FIRST;
     case 2:
-      return 'nd';
+      return NumberSuffix.SECOND;
     case 3:
-      return 'rd';
+      return NumberSuffix.THIRD;
     default:
-      return 'th';
+      return NumberSuffix.OTHER;
   }
 };
