8fc8f52cebd1176a25891f705654a3ed96dfd7b0
Vitalii Astakhov <vitalastakhov@gmail.com>
2023-11-26 20:25:40 +0300

    move analytics-related IDs from _document to .env file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,19 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from "next/document";
+
+const GOOGLE_TAG_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
+const YANDEX_METRIKA_ID = process.env.NEXT_PUBLIC_YANDEX_METRIKA_ID;
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
+const VK_PIXEL_ID = process.env.NEXT_PUBLIC_VK_PIXEL_ID;
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <link rel='manifest' href='/favicon/site.webmanifest' />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <link
-          rel='mask-icon'
-          href='/favicon/safari-pinned-tab.svg'
-          color='#5bbad5'
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
         />
 
         <script
@@ -17,14 +22,14 @@ export default function Document() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WZFZCBZ');`,
+          })(window,document,'script','dataLayer','${GOOGLE_TAG_ID}');`,
           }}
         />
         <script
           dangerouslySetInnerHTML={{
             __html: `!function(){var t=document.createElement("script");
             t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',
-            t.onload=function(){VK.Retargeting.Init("VK-RTRG-1562603-5S6NV"),
+            t.onload=function(){VK.Retargeting.Init("${VK_PIXEL_ID}"),
             VK.Retargeting.Hit()},document.head.appendChild(t)}();`,
           }}
         />
@@ -36,7 +41,7 @@ export default function Document() {
              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-             ym(91124831, "init", {
+             ym(${YANDEX_METRIKA_ID}, "init", {
                   clickmap:true,
                   trackLinks:true,
                   accurateTrackBounce:true,
@@ -54,34 +59,34 @@ export default function Document() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '821428385644561');
+            fbq('init', '${FB_PIXEL_ID}');
             fbq('track', 'PageView');`,
           }}
         />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#dee5c6' />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#dee5c6" />
       </Head>
       <body>
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZFZCBZ"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_ID}"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<img src="https://vk.com/rtrg?p=VK-RTRG-1562603-5S6NV" style="position:fixed; left:-999px;" alt=""/>`,
+            __html: `<img src="https://vk.com/rtrg?p=${VK_PIXEL_ID}" style="position:fixed; left:-999px;" alt=""/>`,
           }}
         />
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<div><img src="https://mc.yandex.ru/watch/91124831" style="position:absolute; left:-9999px;" alt="" /></div>`,
+            __html: `<div><img src="https://mc.yandex.ru/watch/${YANDEX_METRIKA_ID}" style="position:absolute; left:-9999px;" alt="" /></div>`,
           }}
         />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=821428385644561&ev=PageView&noscript=1"
+              src=https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1"
             />`,
           }}
         />
